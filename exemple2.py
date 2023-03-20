6bda66eeb5f5667ce8a0c4c5ce2761a519f909dd
manueldelorme <mintaka2005@gmail.com>
2023-03-20 15:35:44 -0400

    v2

--- exemple2.py
+++ exemple2.py
@@ -3,16 +3,31 @@ import time
 import pygame
 from pygame.locals import *
 
+
 class Joueur:
-    IMAGE = "images/OIP__1_-removebg-preview (1).png"
+    IMAGE = "/Users/manueldelorme/PycharmProjects/pygame/images"
+    TAILLE_BLOC = 44
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.image = pygame.image.load(self.IMAGE).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.TAILLE_BLOC, self.TAILLE_BLOC))
 
     def dessiner(self, surface_jeu):
         surface_jeu.blit(self.image, (self.x, self.y))
 
+    def aller_gauche(self):
+        self.x -= self.TAILLE_BLOC
+
+    def aller_droite(self):
+        self.x += self.TAILLE_BLOC
+
+    def aller_haut(self):
+        self.x -= self.TAILLE_BLOC
+
+    def aller_bas(self):
+        self.x += self.TAILLE_BLOC
 
 
 class App:
@@ -24,9 +39,6 @@ class App:
         self.surface_jeu = pygame.display.set_mode(self.DIMENSION, pygame.HWSURFACE)
         self.jeu_roule = True
         self.joueur = Joueur(self.DIMENSION[0] // 2, self.DIMENSION[1] // 2)
-        self.image = pygame.transform.scale(self.image, (44,44))
-
-
 
     def traiter_input(self):
         for event in pygame.event.get():
@@ -34,6 +46,14 @@ class App:
                 self.jeu_roule = False
             elif event.type == MOUSEMOTION:
                 pass
+            elif event.type == KEYUP and event.KEY == K_LEFT:
+                self.joueur.aller_gauche()
+            elif event.type == KEYUP and event.KEY == K_RIGHT:
+                self.joueur.aller_droite()
+            elif event.type == KEYUP and event.KEY == K_UP:
+                self.joueur.aller_haut()
+            elif event.type == KEYUP and event.KEY == K_DOWN:
+                self.joueur.aller_bas()
             else:
                 print(event)
 
@@ -41,6 +61,7 @@ class App:
         pass
 
     def maj_écran(self):
+        self.surface_jeu.fill((0, 0, 0))
         self.joueur.dessiner(self.surface_jeu)
         pygame.display.flip()
 
@@ -50,7 +71,6 @@ class App:
             self.appliquer_logique()
             self.maj_écran()
             time.sleep(self.DURÉE_FRAME)
-
 
 
 if __name__ == '__main__':
